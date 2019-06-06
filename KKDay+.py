6641da61d09df93e6eeb35dacd1d8574168534c2
junglewill <b04703008@ntu.edu.tw>
2019-06-06 22:09:04 +0800

    v2(未加上日期套件)
    目前完成輸入文字格，還有互相影響的combobox

--- KKDay+.py
+++ KKDay+.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import *
+import tkinter.font as tkFont
 
 class Window(tk.Frame):
 
@@ -9,28 +11,73 @@ class Window(tk.Frame):
 		self.create_widgets()
 		
 	def create_widgets(self):
+		f = tkFont.Font(size = 20, family = "Courier New")
+
+		self.dict = {'泰國': ['芭達雅', '普吉島', '曼谷'],
+                     '日本': ['東京', '沖繩', '大阪', '京都'],
+                     '韓國': ['釜山', '首爾'],
+					 '香港': ['香港'],
+					 '新加坡': ['新加坡']}
+		self.variable1 = tk.StringVar(self)
+		self.variable2 = tk.StringVar(self)
+		self.variable1.set('選擇國家')
+		self.variable2.set('選擇城市')
+		self.variable1.trace('w', self.updateoptions)
+		self.droplist1 = tk.OptionMenu(self, self.variable1, *self.dict.keys(), command=self.getValue)
+		self.droplist2 = tk.OptionMenu(self, self.variable2, '', command=self.getValue)
+
+		default1 = "要的行程"
+		self.txt1 = tk.Entry(self, bd=1)
+		self.txt1.insert(0, default1)
+		self.txt1.bind('<FocusIn>', self.on_entry_click(self.txt1, default1))
+		self.txt1.bind('<FocusOut>', self.on_focusout(self.txt1, default1))
+		self.txt1.config(fg='grey')
+		default2 = "不要的行程"
+		self.txt2 = tk.Entry(self, bd=1)
+		self.txt2.insert(0, default2)
+		self.txt2.bind('<FocusIn>', self.on_entry_click(self.txt2, default2))
+		self.txt2.bind('<FocusOut>', self.on_focusout(self.txt2, default2))
+		self.txt2.config(fg='grey')
+
+		self.btn1 = tk.Button(self, height=2, width=8, text="Search", command=self.clickBtn, font=f)
+		self.cvsMain = tk.Canvas(self, width = 800, height = 600, bg = "white")
 		
-		self.dropdown1 = ttk.Combobox(self , values=["泰國", "日本", "香港", "韓國", "新加坡"])
-		self.dropdown2 = ttk.Combobox(self , values=["北海道", "東京", "沖繩", "大阪", "京都", "首爾", "釜山", "芭達雅", "普吉島" , "曼谷", "新加坡", "香港"])
-		self.txt1 = tk.Text(self, height=1, width=5)
-		self.txt2 = tk.Text(self, height=1, width=5)
-		self.lb1 = tk.Label(self, height=1, width=10, text="要的關鍵字")
-		self.lb2 = tk.Label(self, height=1, width=10, text="不要的關鍵字")
-		self.btn1 = tk.Button(self, height=1, width=5, text="Search", command=self.clickBtn)
 		
-		
-		self.dropdown1.grid(row=0, column=0, columnspan=1,sticky=tk.E+tk.W)
-		self.dropdown2.grid(row=0, column=2, columnspan=1,sticky=tk.E+tk.W)
-		self.txt1.grid(row=2, column=0, columnspan=1,sticky=tk.E+tk.W)
-		self.txt2.grid(row=2, column=2, columnspan=1,sticky=tk.E+tk.W)
-		self.lb1.grid(row=1, column=0, columnspan=3,sticky=tk.W) 
-		self.lb2.grid(row=1, column=2, columnspan=3,sticky=tk.W)
-		self.btn1.grid(row=2, column=4, sticky=tk.E+tk.W)
-		
+		self.droplist1.grid(row=0, column=0, columnspan=2,sticky=tk.E+tk.W)
+		self.droplist2.grid(row=0, column=2, columnspan=2,sticky=tk.E+tk.W)
+		self.txt1.grid(row=1, column=0, columnspan=4,sticky=tk.E+tk.W)
+		self.txt2.grid(row=2, column=0, columnspan=4,sticky=tk.E+tk.W)
+		self.btn1.grid(row=1, column=4, rowspan=2, sticky=tk.E+tk.W)
+		self.cvsMain.grid(row=3, column=0, columnspan=4, sticky= tk.NE + tk.SW)
+
 	def clickBtn(self): 
-		
+		country_to_use = self.variable1.get()
+		city_to_use = self.variable2.get()
+		return country_to_use, city_to_use
+
+	def getValue(self):
+
 		pass		
-		
+
+
+	def on_entry_click(self, entry, text):  # function that gets called whenever entry is clicked
+		if entry.cget('fg') == 'grey':
+			entry.delete(0, "end") # delete all the text in the entry
+			entry.insert(0, '') #Insert blank for user input
+			entry.config(fg = 'black')
+
+	def on_focusout(self, entry, text):
+		if entry.get() == '':
+			entry.insert(0, text)
+			entry.config(fg = 'grey')	
+
+	def updateoptions(self, *args):
+		countries = self.dict[self.variable1.get()]
+		self.variable2.set(countries[0])
+		menu = self.droplist2['menu']
+		menu.delete(0, 'end')
+		for country in countries:
+			menu.add_command(label=country, command=lambda country=country: self.variable2.set(country))
 	
 	
 mywindow = Window()
