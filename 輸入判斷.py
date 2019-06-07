6eb6b9cedd397f3fbc6390f2f8c368c4c58d1a11
riva <b04106016@ntu.edu.tw>
2019-06-07 22:18:17 +0800

    Update 輸入判斷.py

--- 輸入判斷.py
+++ 輸入判斷.py
@@ -6,9 +6,12 @@ csv = csv.DictReader(fh)
 fields = csv.fieldnames
 
 #輸入關鍵字(kw)，用空格分隔後存進kwList
-kwList = input().split()
+kwList = input('請輸入您期望的條件關鍵字：').split()
+noList = input('請輸入您不想要的條件關鍵字：').split()
 
-result = 0 #查找到幾筆結果
+
+resultNum = 0 #找到幾筆符合結果
+resultList = [] #存放符合的結果
 #走訪ouput.csv的info
 for aline in csv:
     count = 0 #檢查找到的kw數
@@ -17,13 +20,22 @@ for aline in csv:
     for kw in kwList:
         if kw in info:
             count += 1
+#走訪noList中不想要的關鍵字，若出現則開始比對下一筆行程            
+    for no in noList:
+        if no in info:
+            count -= 1
+        
     if count == len(kwList):
-        result += 1
-        print('行程名稱：' + aline[fields[2]])
-        print('價格：' + aline[fields[3]])
-        print('介紹：' + aline[fields[4]])
-        print('連結：' + 'https://www.kkday.com/zh-tw/product/' + aline[fields[1]])
+        resultNum += 1
+        result = [] #存放符合結果的內容
+        result.append('行程名稱：' + aline[fields[2]])
+        result.append('價格：' + aline[fields[3]])
+        result.append('介紹：' + aline[fields[4]])
+        result.append('連結：' + 'https://www.kkday.com/zh-tw/product/' + aline[fields[1]])
+        resultList.append(result)
 
-if result == 0:
+if resultNum == 0:
     print('查無符合的結果！')
 
+#for r in resultList:
+    #print(r)
