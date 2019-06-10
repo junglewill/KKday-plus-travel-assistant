6e765b3fea4a5f7b675846be80f267ca0a3c25f7
riva <b04106016@ntu.edu.tw>
2019-06-10 12:34:44 +0800

    Update 輸入判斷.py

--- 輸入判斷.py
+++ 輸入判斷.py
@@ -9,30 +9,35 @@ fields = csv.fieldnames
 kwList = input('請輸入您期望的條件關鍵字：').split()
 noList = input('請輸入您不想要的條件關鍵字：').split()
 
-
+#SelectCity = '沖繩' #測試時打開
 resultNum = 0 #找到幾筆符合結果
 resultList = [] #存放符合的結果
 #走訪ouput.csv的info
 for aline in csv:
-    count = 0 #檢查找到的kw數
-    info = aline[fields[4]]
-#走訪kwList中的關鍵字，查看是否皆出現在某行程的'info'中
-    for kw in kwList:
-        if kw in info:
-            count += 1
-#走訪noList中不想要的關鍵字           
-    for no in noList:
-        if no in info:
-            count -= 1
-        
-    if count == len(kwList):
-        resultNum += 1
-        result = [] #存放符合結果的內容
-        result.append('行程名稱：' + aline[fields[2]])
-        result.append('價格：' + aline[fields[3]])
-        result.append('介紹：' + aline[fields[4]])
-        result.append('連結：' + 'https://www.kkday.com/zh-tw/product/' + aline[fields[1]])
-        resultList.append(result)
+    if SelectCity == aline[fields[1]]: 
+        count = 0 #檢查找到的kw數
+        info = aline[fields[5]]
+    #走訪kwList中的關鍵字，查看是否皆出現在某行程的'info'中
+        if len(kwList) == 0 and len(noList) == 0:
+            count = len(kwList)
+
+        else:
+            for kw in kwList:
+                if kw in info:
+                    count += 1
+        #走訪noList中不想要的關鍵字           
+            for no in noList:
+                if no in info:
+                    count -= 1
+            
+        if count == len(kwList):
+            resultNum += 1
+            result = [] #存放符合結果的內容
+            result.append('行程名稱：' + aline[fields[3]])
+            result.append('價格：' + aline[fields[4]])
+            result.append('介紹：' + aline[fields[5]])
+            result.append('連結：' + 'https://www.kkday.com/zh-tw/product/' + aline[fields[2]])
+            resultList.append(result)
 
 if resultNum == 0:
     print('查無符合的結果！')
